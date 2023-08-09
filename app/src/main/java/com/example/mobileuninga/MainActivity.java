a9f901053dd58946502cf9a5eb5871bbe52f709c
Camila <127354836+camila-frimmel@users.noreply.github.com>
2023-08-09 11:52:57 -0300

    commit 3

--- app/src/main/java/com/example/mobileuninga/MainActivity.java
+++ app/src/main/java/com/example/mobileuninga/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.mobileuninga;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+private Button formButton;
+private Button listButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        @Override
+protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        formButton = findViewById(R.id.formButton);
+        listButton = findViewById(R.id.listButton);
+
+        formButton.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+                openFormActivity();
+                }
+});
+
+        listButton.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+                openListActivity();
+                }
+ });
+        }
+
+        private void openFormActivity() {
+        Intent intent = new Intent(this, FormActivity.class);
+        startActivity(intent);
+        }
+
+        private void openListActivity() {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
+        }
 }
