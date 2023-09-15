6ed3a8131fa065f2830d1a19184b9249b0f0a9d2
Camila <127354836+camila-frimmel@users.noreply.github.com>
2023-09-15 15:43:52 -0300

    commit 9

--- app/src/main/java/com/example/mobileuninga/FormActivity.java
+++ app/src/main/java/com/example/mobileuninga/FormActivity.java
@@ -1,15 +1,38 @@
 package com.example.mobileuninga;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class FormActivity extends AppCompatActivity {
+public class FormActivity extends MainActivity {
 
-        @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
-        }
+
+        Button botao = (Button)findViewById(R.id.button);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BancoController crud = new BancoController(getBaseContext());
+                EditText nome = (EditText)findViewById(R.id.editText);
+                EditText CPF = (EditText)findViewById((R.id.editText2));
+                EditText telefone = (EditText)findViewById(R.id.editText3);
+                String tituloString = nome.getText().toString();
+                String autorString = CPF.getText().toString();
+                String editoraString = telefone.getText().toString();
+                String resultado;
+
+                resultado = crud.FormActivity(tituloString,autorString,editoraString);
+
+                Toast.makeText(getApplicationContext(), resultado, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 }
