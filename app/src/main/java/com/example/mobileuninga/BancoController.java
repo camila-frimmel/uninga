2e43a5a073d4a5ec1e12ee67419cd9452d55c080
Camila <127354836+camila-frimmel@users.noreply.github.com>
2023-09-15 15:25:17 -0300

    commit 6

---
+++ app/src/main/java/com/example/mobileuninga/BancoController.java
@@ -0,0 +1,87 @@
+package com.example.mobileuninga;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class BancoController {
+
+    private SQLiteDatabase db;
+    private CriaBanco banco;
+
+    public BancoController(Context context){
+        banco = new CriaBanco(context);
+    }
+
+    public String FormActivity(String titulo, String autor, String editora){
+        ContentValues valores;
+        long resultado;
+
+        db = banco.getWritableDatabase();
+        valores = new ContentValues();
+        valores.put(CriaBanco.NOME, titulo);
+        valores.put(CriaBanco.CPF, autor);
+        valores.put(CriaBanco.TELEFONE, editora);
+
+        resultado = db.insert(CriaBanco.TABELA, null, valores);
+        db.close();
+
+        if (resultado ==-1)
+            return "Erro ao inserir registro";
+        else
+            return "Registro Inserido com sucesso";
+
+    }
+
+    public Cursor carregaDados(){
+        Cursor cursor;
+        String[] campos =  {banco.ID,banco.NOME};
+        db = banco.getReadableDatabase();
+        cursor = db.query(banco.TABELA, campos, null, null, null, null, null, null);
+
+        if(cursor!=null){
+            cursor.moveToFirst();
+        }
+        db.close();
+        return cursor;
+    }
+
+    public Cursor carregaDadoById(int id){
+        Cursor cursor;
+        String[] campos =  {banco.ID,banco.NOME,banco.CPF,banco.TELEFONE};
+        String where = CriaBanco.ID + "=" + id;
+        db = banco.getReadableDatabase();
+        cursor = db.query(CriaBanco.TABELA,campos,where, null, null, null, null, null);
+
+        if(cursor!=null){
+            cursor.moveToFirst();
+        }
+        db.close();
+        return cursor;
+    }
+
+    public void alteraRegistro(int id, String nome, String CPF, String telefone){
+        ContentValues valores;
+        String where;
+
+        db = banco.getWritableDatabase();
+
+        where = CriaBanco.ID + "=" + id;
+
+        valores = new ContentValues();
+        valores.put(CriaBanco.NOME, nome);
+        valores.put(CriaBanco.CPF, CPF);
+        valores.put(CriaBanco.TELEFONE, telefone);
+
+        db.update(CriaBanco.TABELA,valores,where,null);
+        db.close();
+    }
+
+    public void deletaRegistro(int id){
+        String where = CriaBanco.ID + "=" + id;
+        db = banco.getReadableDatabase();
+        db.delete(CriaBanco.TABELA,where,null);
+        db.close();
+    }
+}
