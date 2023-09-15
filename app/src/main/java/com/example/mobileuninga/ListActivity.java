8a9b2f54f7f407de7e93baa9b5e9f280e50636c1
Camila <127354836+camila-frimmel@users.noreply.github.com>
2023-09-15 15:45:47 -0300

    commit 10

--- app/src/main/java/com/example/mobileuninga/ListActivity.java
+++ app/src/main/java/com/example/mobileuninga/ListActivity.java
@@ -1,14 +1,44 @@
 package com.example.mobileuninga;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class ListActivity extends AppCompatActivity {
 
-        @Override
+public class ListActivity extends Activity {
+    private ListView lista;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        }
+
+        BancoController crud = new BancoController(getBaseContext());
+        Cursor cursor = crud.carregaDados();
+
+        String[] nomeCampos = new String[] {CriaBanco.ID, CriaBanco.NOME};
+        int[] idViews = new int[] {R.id.idPessoa, R.id.nome};
+
+        SimpleCursorAdapter adaptador = new SimpleCursorAdapter(getBaseContext(),
+                R.layout.activity_list,cursor,nomeCampos,idViews, 0);
+        lista = (ListView)findViewById(R.id.listView);
+        lista.setAdapter(adaptador);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String codigo;
+                cursor.moveToPosition(position);
+                codigo = cursor.getString(cursor.getColumnIndexOrThrow(CriaBanco.ID));
+                Intent intent = new Intent(ListActivity.this, Alterar.class);
+                intent.putExtra("codigo", codigo);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 }
